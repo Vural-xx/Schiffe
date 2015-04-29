97fb72128becadc4e9b7294d180c48846828e40c
Vural <vy@i2dm.de>
2015-04-29 21:39:57 +0200

    Persistieren von Objekten angefangen

---
+++ src/main/java/de/hs/bremen/persistence/FilePersistenceManager.java
@@ -0,0 +1,45 @@
+package de.hs.bremen.persistence;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class FilePersistenceManager implements PersistenceManager {
+
+	private BufferedReader reader = null;
+	private PrintWriter writer = null;
+	
+	public void openForReading(String datei) throws FileNotFoundException {
+		reader = new BufferedReader(
+					new FileReader(datei));
+	}
+
+	public void openForWriting(String datei) throws IOException {
+		writer = new PrintWriter(
+					new BufferedWriter(
+						new FileWriter(datei)));
+	}
+
+	public boolean close() {
+		if (writer != null)
+			writer.close();
+		
+		if (reader != null) {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
---
+++ src/main/java/de/hs/bremen/persistence/PersistenceManager.java
@@ -0,0 +1,31 @@
+package de.hs.bremen.persistence;
+
+import java.io.IOException;
+
+/**
+ * Interface zum speichern von Objekten
+ * @author vural
+ *
+ */
+public interface PersistenceManager {
+
+	/**
+	 * Öffnet eine Datei zum Lesen.
+	 * @param datenquelle: Datenquelle die gelesen werden soll.
+	 * @throws IOException: Input/Output Exception
+	 */
+	public void openForReading(String datenquelle) throws IOException;
+	
+	/** Öffnet eine Datei zum Speichern.
+	 * @param datenquelle: Datenqeulle in der gespeichert werden soll.
+	 * @throws IOException: Input/Output Exception
+	 */
+	public void openForWriting(String datenquelle) throws IOException;
+	
+	/**
+	 * Gibt an ob der Stream geschlossen wurde.
+	 * @return boolean 
+	 */
+	public boolean close();
+	
+}
