a2495f3b50f1174422c9f7bae6e76b9a097afdb6
Vural <vy@i2dm.de>
2015-04-30 13:58:27 +0200

    Revert 6b12dbb..d98cc39
    This rolls back to commit 6b12dbb2083694ce8c3f38d668e7db84a618f316.

--- src/main/java/de/hs/bremen/model/Feld.java
+++ src/main/java/de/hs/bremen/model/Feld.java
@@ -12,7 +12,6 @@ public class Feld {
 		inhalt = Spiel.ANSI_BLUE+"~" + Spiel.ANSI_RESET;
 		feldstatus = Feldstatus.WASSER;
 		this.position = position;
-		this.position = position;
 	}
 	
 	public Position getPosition() {
