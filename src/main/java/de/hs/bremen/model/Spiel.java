b6ca4bd12a7ce8f0e94078069b52aa331cea06e6
Vural <vy@i2dm.de>
2015-05-12 09:25:04 +0200

    Speicher und Lademenü
    Bisher ohne Funktion (nur Menü)

--- src/main/java/de/hs/bremen/model/Spiel.java
+++ src/main/java/de/hs/bremen/model/Spiel.java
@@ -1,6 +1,7 @@
 package de.hs.bremen.model;
 
 
+import java.io.File;
 import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import de.hs.bremen.enums.Feldstatus;
+import de.hs.bremen.persistence.ObjectPersistenceManager;
 import helper.IO;
 
 /**
@@ -311,6 +313,47 @@ public class Spiel implements Serializable {
 		
 	}
 	
+	public void ladeMenu(){
+		int spielstand;
+		String ladeMenu = "";
+		System.out.println("Bitte geben Sie an welchen Spielstand Sie laden möchten.");
+		ObjectPersistenceManager opm = new ObjectPersistenceManager();
+		File folder = new File("src/temp");
+		File[] listOfFiles = folder.listFiles();
+		
+		for(int i = 0; i < listOfFiles.length; i++){
+			ladeMenu = ladeMenu +"Stand " +(i+1)+ ": " + listOfFiles[i].getName()+ " \t";
+		}
+		System.out.println(ladeMenu);
+		spielstand = IO.readInt();
+		
+		Spiel spiel = opm.spielstandLaden(listOfFiles[spielstand-1].getName());
+		System.out.println("Geschafft");
+		
+	}
+	
+	public void speicherMenu(){
+		int spielstand;
+		String saveMenu = "";
+		System.out.println("Bitte geben Sie an welchen Spielstand sie überschreiben oder neu schreiben möchten");
+		ObjectPersistenceManager opm = new ObjectPersistenceManager();
+		File folder = new File("src/temp");
+		File[] listOfFiles = folder.listFiles();
+		
+		for(int i = 0; i < listOfFiles.length; i++){
+			saveMenu = saveMenu +"Stand " +(i+1)+ ": " + listOfFiles[i].getName()+ " \t";
+		}
+		
+		if(listOfFiles.length< 4){
+			for(int i = listOfFiles.length; i < 4; i++){
+				saveMenu = saveMenu +"Stand " +(i+1)+ ": LEER \t";
+			}
+		}
+		System.out.println(saveMenu);
+		spielstand = IO.readInt();
+		
+	}
+
 	
 	public void schiffeAufFeldSetzen(){
 		for (int i = 0 ; i< spieler.length; i++){		
@@ -425,7 +468,7 @@ public class Spiel implements Serializable {
 	
 	public static void main(String[] args){
 		Spiel spiel = new Spiel();
-		spiel.init();	
+		spiel.init();
 	}
 	
 
