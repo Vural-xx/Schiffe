372854728504040cf1fddc1527fde4e1370067df
bab00n87 <flomeinert@gmail.com>
2015-06-06 12:59:39 +0200

    spieler Name position fixed

--- src/main/java/de/hs/bremen/gui/spielerNameGUI.java
+++ src/main/java/de/hs/bremen/gui/spielerNameGUI.java
@@ -3,7 +3,10 @@ package de.hs.bremen.gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -17,6 +20,7 @@ public class spielerNameGUI extends JPanel {
 	
 	public JPanel container1;
 	public JTextField spielerEingabe;
+	public GridBagConstraints gbc= new GridBagConstraints();
 
 	
 
@@ -39,17 +43,10 @@ public class spielerNameGUI extends JPanel {
 	
 	public void initComponents(){
 		
-		this.setLayout(new BorderLayout(0, 100));
-		flowLayout = new FlowLayout();
-		container1 = new JPanel();
-		spielernameAnfangsText= new JLabel("Bitte geben Sie ihren Spielernamen ein:");
-		container1.setLayout(new GridLayout(0,1));
-	
-		
-
-		
-		this.add(spielernameAnfangsText, BorderLayout.PAGE_START);
-		this.add(container1);
+		this.setLayout(new GridBagLayout());
+		gbc.gridx=0;
+		gbc.gridx=1;
+		gbc.insets=new Insets(20, 0, 0, 0);
 
 		
 		
@@ -68,8 +65,8 @@ public class spielerNameGUI extends JPanel {
 			
 			spielerNametext= new JLabel("Spielername " + i+": ");
 		
-			container1.add(spielerNametext);
-			container1.add(spielerEingabe);
+			this.add(spielerNametext,gbc);
+			this.add(spielerEingabe,gbc);
 			
 			
 			
