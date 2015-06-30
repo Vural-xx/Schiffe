724b5b3e0f19c0b26e0cae8661155de3e0bd2f26
Vural <vy@i2dm.de>
2015-06-30 11:02:28 +0200

    Aufteilung von Spielfeld in Public und private Klasse
    Erste Idee, noch nicht ins laufende Spiel eingebaut

---
+++ src/main/java/de/hs/bremen/model/SpielfeldPublic.java
@@ -0,0 +1,43 @@
+package de.hs.bremen.model;
+
+import de.hs.bremen.abstracts.AbstractSpielfeld;
+import de.hs.bremen.enums.Feldstatus;
+
+public class SpielfeldPublic extends AbstractSpielfeld {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7038045028562697796L;
+
+	public SpielfeldPublic(int groesse) {
+		super(groesse);
+	}
+	
+	/**
+	 * Platziert Feuer auf Feld, die der Position angehört.
+	 * @param position: Position auf dessen Feld gefeuert werden soll.
+	 */
+	public void feuerPlatzieren(Position position){
+		for(int i = 0 ; i < felder.length; i++){
+			for (int j = 0 ; j < felder[i].length; j++){
+				if(felder[i][j].getPosition().equals(position) && felder[i][j].getFeldstatus() == Feldstatus.WASSER){
+						felder[i][j].setFeldstatus(Feldstatus.VERFEHLT);
+				}
+		    }
+		}
+	}
+	
+	/**
+	 * Platziert Feuer auf Feldern, die den Positionen angehören.
+	 * @param position: Position auf dessen Feld gefeuert werden soll.
+	 */
+	public void feuerPlatzieren(Position[] position){
+		for(int i = 0; i <position.length; i++){
+			if(position[i] != null){
+				feuerPlatzieren(position[i]);
+			}
+		}
+	}
+
+}
