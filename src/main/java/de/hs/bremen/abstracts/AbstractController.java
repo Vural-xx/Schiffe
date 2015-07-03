13e548f4adbc1eeb87e1b16af329a70f5325a52c
Vural <vy@i2dm.de>
2015-07-03 13:54:23 +0200

    Spiel zum Speichern umgeschrieben

--- src/main/java/de/hs/bremen/abstracts/AbstractController.java
+++ src/main/java/de/hs/bremen/abstracts/AbstractController.java
@@ -1,21 +1,19 @@
 package de.hs.bremen.abstracts;
 
-import de.hs.bremen.enums.Spielerart;
 import de.hs.bremen.gui.MainFrame;
 import de.hs.bremen.model.Actor;
-import de.hs.bremen.model.ComputerGegner;
-import de.hs.bremen.model.Spieler;
+import de.hs.bremen.model.Spiel;
 
 
 public class AbstractController {
-	private Actor[] spieler;
+	private Spiel spiel;
 	private MainFrame mainFrame;
 	
 	public Actor[] getSpieler() {
-		return spieler;
+		return spiel.getSpieler();
 	}
 	public void setSpieler(Actor[] spieler) {
-		this.spieler = spieler;
+		this.spiel.setSpieler(spieler);
 	}
 
 	public AbstractController(){
